d99fb370eb5fd0e71c05a1269162f0d63d208c1d
shruti0018 <65651136+shruti0018@users.noreply.github.com>
2020-05-26 16:07:44 +0530

    newchanges1

--- app/src/main/java/com/example/mh_app/MainActivity.java
+++ app/src/main/java/com/example/mh_app/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.mh_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button divein;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        divein =findViewById(R.id.DiveIn);
+        divein.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveToActivityTwo();
+            }
+        });
+
+    }
+
+    private  void moveToActivityTwo(){
+        Intent intent=new Intent(MainActivity.this,MH2.class);
+        startActivity(intent);
     }
 }
