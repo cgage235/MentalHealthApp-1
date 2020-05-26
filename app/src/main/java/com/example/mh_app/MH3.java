f1a2ea52759733dec32b4e30e14bf5ce2ac03627
shruti0018 <65651136+shruti0018@users.noreply.github.com>
2020-05-26 18:25:57 +0530

    newchanges5

--- app/src/main/java/com/example/mh_app/MH3.java
+++ app/src/main/java/com/example/mh_app/MH3.java
@@ -2,13 +2,33 @@ package com.example.mh_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MH3 extends AppCompatActivity {
+    private Button signin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_m_h3);
+
+
+        signin =findViewById(R.id.SignIn);
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveToActivityTwo();
+            }
+        });
+
+    }
+
+    private  void moveToActivityTwo(){
+        Intent intent=new Intent(MH3.this,MH2.class);
+        startActivity(intent);
     }
 }
+
