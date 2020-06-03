a5ac7ce373d2e7b82251e456d6d34243e81a08ee
shruti0018 <65651136+shruti0018@users.noreply.github.com>
2020-06-04 01:17:09 +0530

    newchanges

--- app/src/main/java/com/example/mh_app/NotesText.java
+++ app/src/main/java/com/example/mh_app/NotesText.java
@@ -2,7 +2,11 @@ package com.example.mh_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 
 public class NotesText extends AppCompatActivity {
 
@@ -10,5 +14,36 @@ public class NotesText extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes_text);
+
+        EditText editText = findViewById(R.id.notesId);
+
+        Intent intent = getIntent();
+        final int noteId = intent.getIntExtra("noteId",-1);
+
+        if (noteId != -1) {
+            editText.setText(MH5.notes.get(noteId));
+        } //else {
+          //  MH5.notes.add("");
+          //  noteId = MH5.notes.size() - 1;
+       // }
+    editText.addTextChangedListener(new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            MH5.notes.set(noteId, String.valueOf(s));
+            MH5.arrayAdapter.notifyDataSetChanged();
+
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    });
     }
 }
